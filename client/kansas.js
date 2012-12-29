9f7980198f682e3d5c7476ac48103b5d3b6b6c1b
Eric Liang <ericliang@berkeley.edu>
2012-12-29 04:27:03 -0800

    split TODOs into separate file

--- client/kansas.js
+++ client/kansas.js
@@ -13,29 +13,6 @@
  * which again will be received by other clients in a globally consistent order.
  */
 
-// High prio, perf:
-// TODO consider changing images / reusing nodes to impl fast z-index changes
-// TODO investigate patching jquery ui to speed up selects (reuse the helper?)
-// TODO send moves in bulk requests when possible
-
-// High prio, ui:
-// TODO better stack rendering, so that cards in 2/3/4 stacks are easily seen
-// TODO make it so that two "untaps" or "taps" don't result in a no-op
-// TODO make sure that toCanonicalKey and toClientKey implement a bijection
-
-// Low prio, ui:
-// TODO "compress" the opponent's side so that you can have more vertical space
-// TODO have decks on opponent's side stack the opposite direction?
-// TODO bind frames to cards so that when cards move the frame still is correct
-// TODO allow dragging to other users hands + browsing hands as a selection
-// TODO implement creating/destroying cards on the board
-// TODO garbage collect frames / unfade cards after a period of no activity
-// TODO allow hand sorting, or autosorting by some property
-// TODO some way of choosing user id and game id
-// TODO different colors for different user frames
-// TODO save games in localstorage
-// TODO investigate using phonegap - can have back button close hovermenu?
-
 // Default settings for websocket connection.
 var kWSPort = 8080
 var gameid = "testgame1";
