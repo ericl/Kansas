0349217b8f61d88477c504f12b4fcb016437924c
Eric Liang <ericliang@berkeley.edu>
2013-01-29 00:44:07 -0800

    make Image module optional

--- server/kansas_wsh.py
+++ server/kansas_wsh.py
@@ -1,6 +1,5 @@
 # Implementation of Kansas websocket handler.
 
-import Image
 import copy
 import collections
 import json
@@ -11,6 +10,13 @@ import threading
 import time
 import urllib2
 import decks
+
+try:
+    import Image
+    haveImaging = True
+except:
+    logging.warning("Failed to import imaging module.")
+    haveImaging = False
 
 kSmallImageSize = (123, 175)
 kServingPrefix = ''
@@ -51,7 +57,7 @@ class CachingLoader(dict):
             # Generates small version of images.
             small_path = large_path[:-4] + ('@%dx%d.jpg' % kSmallImageSize)
             if not os.path.exists(small_path):
-                self.resize(large_path, small_path)
+                small_path = self.resize(large_path, small_path)
             self['urls_small'][card] = small_path
 
         # Caches the back image.
@@ -68,10 +74,14 @@ class CachingLoader(dict):
 
     def resize(self, large_path, small_path):
         """Resizes image found at large_path and saves to small_path."""
-        logging.info("Resize %s -> %s" % (large_path, small_path))
-        Image.open(large_path)\
-             .resize(kSmallImageSize, Image.ANTIALIAS)\
-             .save(small_path)
+        if haveImaging:
+            logging.info("Resize %s -> %s" % (large_path, small_path))
+            Image.open(large_path)\
+                 .resize(kSmallImageSize, Image.ANTIALIAS)\
+                 .save(small_path)
+            return small_path
+        else:
+            return large_path
 
     def toAbsoluteURL(self, url):
         if url.startswith('/'):
