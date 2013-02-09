5ddfef6790c144f3a560b27d9e295931ca4e5945
Eric Liang <ericliang@berkeley.edu>
2013-02-09 00:01:22 -0800

    fix up temp staging script

--- makedeck.py
+++ makedeck.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python
+# TODO get rid of me
 
+import pprint
 import urllib2
+import sys
 import re
-
-DECK = """
-4 Boartusk Liege
-4 Flinthoof Boar
-4 Gristleback
-4 Krosan Tusker
-4 Pygmy Razorback
-
-2 Boar Umbra
-2 Cultivate
-4 Groundswell
-4 Rampant Growth
-4 Lightning Bolt
-
-14 Forest
-6 Mountain
-4 Terramorphic Expanse
-"""
+from m_deck import DECK
 
 def name_to_url(name):
   req = urllib2.Request("http://magiccards.info/query?q=!%s&v=card&s=cname" % '+'.join(name.split()))
@@ -28,15 +14,46 @@ def name_to_url(name):
   match = re.search('"http://magiccards.info/scans/en/[a-z0-9]*/[a-z0-9]*.jpg"', data)
   return match.group()[33:-1]
 
-i = 60
-for line in DECK.strip().split('\n'):
+i = 0
+buf = {}
+while len(buf) < 60:
+  print "> Enter card specifier (e.g. 4 Mountain):",
+  line = raw_input().strip()
+  if line == 'ok':
+    break
   if line:
-    num, name = line.split(' ', 1)
-    num = int(num)
     try:
+      num, name = line.split(' ', 1)
+      num = int(num)
       url = name_to_url(name)
       for _ in range(num):
-        print '%d: "%s",' % (i, url)
+        buf[i] = url
         i += 1
+      print "\r\nFound %s, deck now has %d cards." % (url, len(buf))
     except Exception, e:
-       print "failed", e
+     print "Could not find '%s'" % line
+
+if len(buf) != 60:
+  print "Deck must have exactly 60 cards. Sorry."
+  exit()
+
+name = None
+while not name:
+  print "> Name your deck:",
+  name = raw_input().strip()
+choice = None
+while choice not in ['a', 'b']:
+  print "These are the current decks."
+  print "  a) %s" % DECK['_deck0']
+  print "  b) %s" % DECK['_deck1']
+  print "> Choose deck to replace:",
+  choice = raw_input().strip()
+if choice == 'a':
+  DECK['_deck0'] = name
+  offset = 0
+else:
+  DECK['_deck1'] = name
+  offset = 60
+for i, v in buf.iteritems():
+  DECK['urls'][i + offset] = v
+pprint.pprint(DECK, open('m_deck.py', 'w'))
