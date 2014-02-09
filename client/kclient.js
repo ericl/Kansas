8cb4a7fd43ea2e362a276a0176fd3b8c03817ede
Eric Liang <ekhliang@gmail.com>
2014-02-09 00:57:15 -0800

    bumped client version to 10

--- client/kclient.js
+++ client/kclient.js
@@ -50,7 +50,7 @@
  *          .commit();
  */
 
-var kClientVersion = 9;  // keep in sync with config.py
+var kClientVersion = 10;  // keep in sync with config.py
 
 function checkVersion(required) {
     if (required && required != kClientVersion) {
