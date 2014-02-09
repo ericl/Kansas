cd4f7d42d206498839dbacb3ade7bf32ab9c40c9
Eric Liang <ekhliang@gmail.com>
2014-02-08 22:07:05 -0800

    bump version

--- client/kclient.js
+++ client/kclient.js
@@ -50,7 +50,7 @@
  *          .commit();
  */
 
-var kClientVersion = 4;  // keep in sync with config.py
+var kClientVersion = 5;  // keep in sync with config.py
 
 function checkVersion(required) {
     if (required && required != kClientVersion) {
