499d6c3e7e1823001cfd7efba35a3789cb298dc1
Eric Liang <ekhliang@gmail.com>
2014-02-08 21:52:04 -0800

    fix cookie support

--- client/localstore.js
+++ client/localstore.js
@@ -2,29 +2,14 @@
 
 var localstore = {};
 
-localstore._load = function() {
-    try {
-        return JSON.parse(document.cookie);
-    } catch (err) {
-        return {};
-    }
-}
-
-localstore._save = function(dict) {
-    document.cookie = JSON.stringify(dict);
-}
-
 localstore.put = function(key, value) {
-    var dict = localstore._load();
-    dict[key] = value;
-    localstore._save(dict);
+    $.cookie(key, value);
 }
 
 localstore.get = function(key, defaultValue) {
-    var dict = localstore._load();
-    var result = dict[key];
-    if (!result) {
-        result = defaultValue;
+    var result = $.cookie(key);
+    if (result === undefined) {
+        return defaultValue;
     }
     return result;
 }
