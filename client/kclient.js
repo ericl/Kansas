eeac183e38811a61d5d2c78e7351cb8d29f15f5c
Eric Liang <ekhliang@gmail.com>
2014-02-09 16:21:45 -0800

    font

--- client/kclient.js
+++ client/kclient.js
@@ -50,7 +50,7 @@
  *          .commit();
  */
 
-var kClientVersion = 25;  // keep in sync with config.py
+var kClientVersion = 26;  // keep in sync with config.py
 
 function checkVersion(required) {
     if (required && required != kClientVersion) {
