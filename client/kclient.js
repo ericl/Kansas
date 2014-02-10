640141ae4ecd40c3c5fe505adad4a103448fce59
Eric Liang <ekhliang@gmail.com>
2014-02-10 03:28:36 -0800

    more blacklist

--- client/kclient.js
+++ client/kclient.js
@@ -50,7 +50,7 @@
  *          .commit();
  */
 
-var kClientVersion = 40;  // keep in sync with config.py
+var kClientVersion = 41;  // keep in sync with config.py
 
 function checkVersion(required) {
     if (required && required != kClientVersion) {
