e079f02fc94fb8a98faa2c844ff3a7e42f4ddf60
Eric Liang <ekhliang@gmail.com>
2014-02-09 16:23:09 -0800

    fix selection

--- client/kclient.js
+++ client/kclient.js
@@ -50,7 +50,7 @@
  *          .commit();
  */
 
-var kClientVersion = 26;  // keep in sync with config.py
+var kClientVersion = 27;  // keep in sync with config.py
 
 function checkVersion(required) {
     if (required && required != kClientVersion) {
