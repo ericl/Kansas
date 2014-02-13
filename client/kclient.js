e6dd93cf532bcb0a3224fe21d109a9aecd1f55dc
Eric Liang <ekhliang@gmail.com>
2014-02-13 12:07:35 -0800

    invalidate cache

--- client/kclient.js
+++ client/kclient.js
@@ -50,7 +50,7 @@
  *          .commit();
  */
 
-var kClientVersion = 67;  // keep in sync with config.py
+var kClientVersion = 68;  // keep in sync with config.py
 
 function checkVersion(required) {
     if (required && required != kClientVersion) {
