78c8d8beda43ddb74c35462b43f3a9cc246ca396
Eric Liang <ekhliang@gmail.com>
2014-04-03 02:02:09 -0700

    Really remove newlines.

--- client/kclient.js
+++ client/kclient.js
@@ -50,7 +50,7 @@
  *          .commit();
  */
 
-var kClientVersion = 153;  // keep in sync with config.py
+var kClientVersion = 154;  // keep in sync with config.py
 var versionRequired = kClientVersion;
 
 function doCheckPopup() {
--- server/config.py
+++ server/config.py
@@ -4,7 +4,7 @@ kSmallImageSize = (92, 131)
 kServingPrefix = ''
 kLocalServingAddress = 'http://localhost:8000/'
 kCachePath = '../cache'
-kClientVersion = 153
+kClientVersion = 154
 kDBPath = '../db'
 
 if not os.path.exists(kCachePath):
